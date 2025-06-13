d49f18a16600aa5b80c8d0ad9d1aebe4864a3318
Federico <127424229+FedevPe@users.noreply.github.com>
2025-06-13 15:20:59 -0300

    Modificación del codigo de MainLayout

---
+++ FincaFenix.UserInterface/Layout/MainLayout.razor.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Components.Routing;
+using MudBlazor;
+
+namespace FincaFenix.UserInterface.Layout
+{
+    public partial class MainLayout
+    {
+        private Breakpoint _breakpoint = Breakpoint.Xs;
+        private DrawerClipMode _clipMode = DrawerClipMode.Never;
+        bool _drawerOpen = true;
+        private bool _showAddOrderButton = false;
+
+        protected override void OnInitialized(){
+            NavManager.LocationChanged += OnLocationChanged;
+            TextAppBar.OnChange += StateHasChanged;
+            UpdateButtonVisibility(NavManager.Uri);
+        }
+        private void OnLocationChanged(object? sender, LocationChangedEventArgs e){
+            UpdateButtonVisibility(e.Location);
+            StateHasChanged();
+        }
+        private void UpdateButtonVisibility(string uri){
+            var relativeUri = NavManager.ToBaseRelativePath(uri).ToLower();
+            _showAddOrderButton = relativeUri.StartsWith("ordenes");
+        }
+        public void Dispose(){
+            NavManager.LocationChanged -= OnLocationChanged;
+            TextAppBar.OnChange -= StateHasChanged;
+        }
+        void DrawerToggle(){
+            _drawerOpen = !_drawerOpen;
+        }
+        MudTheme CustomTheme = new MudTheme(){
+            PaletteLight = new PaletteLight()
+            {
+                Primary = Colors.Teal.Darken4,
+                AppbarBackground = Colors.Teal.Darken4,
+                Secondary = Colors.Teal.Darken3,
+                Tertiary = Colors.Gray.Lighten5,
+                Info = Colors.BlueGray.Default
+            }
+        };
+    }
+}
