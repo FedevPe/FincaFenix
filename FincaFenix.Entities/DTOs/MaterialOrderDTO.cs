f99ca90894dda5fe8e506d41b2dc6e7a7bb6e43e
Federico <afedeperez2021@gmail.com>
2025-06-17 18:55:07 -0300

    Nuevo componente del formulario Crear Orden
    Nuevo DTO, y nuevo componente del formulario Crear Orden

---
+++ FincaFenix.Entities/DTOs/MaterialOrderDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FincaFenix.Entities.DTOs
+{
+    public class MaterialOrderDTO
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public char? Unit { get; set; }
+    }
+}
