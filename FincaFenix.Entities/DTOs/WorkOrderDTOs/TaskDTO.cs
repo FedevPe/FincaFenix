d54c03347cbb6b36ea112c7ba52ce0914b534071
Federico <127424229+FedevPe@users.noreply.github.com>
2025-06-19 13:51:13 -0300

    DTO de tares

---
+++ FincaFenix.Entities/DTOs/WorkOrderDTOs/TaskDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FincaFenix.Entities.DTOs.WorkOrderDTOs
+{
+    public class TaskDTO
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+    }
+}
