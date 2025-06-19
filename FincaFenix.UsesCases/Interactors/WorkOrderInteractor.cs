12c89af253fdb4baf3105a0874ec2cc1678d0217
Federico <127424229+FedevPe@users.noreply.github.com>
2025-06-19 16:07:57 -0300

    Creacion de intereactor

---
+++ FincaFenix.UsesCases/Interactors/WorkOrderInteractor.cs
@@ -0,0 +1,20 @@
+﻿using FincaFenix.Entities.DTOs.WorkOrderDTOs;
+using FincaFenix.UsesCases.Interfaces.WorkOrder;
+using FincaFenix.UsesCases.Repository;
+
+namespace FincaFenix.UsesCases.Interactors
+{
+    public class WorkOrderInteractor(
+        IWorkOrderOuputPort presenter,
+#pragma warning disable CS9113 // El parámetro no está leído.
+        IWorkOrderRepository repository) : IWorkOrderInputPort
+#pragma warning restore CS9113 // El parámetro no está leído.
+    {
+        public async Task Handle(WorkOrderDTO workOrder)
+        {
+            //Lógica para convertir dto a agregado.
+            //await repository.AddWorkOrder(workOrder); 
+            await presenter.Handle(workOrder);
+        }
+    }
+}
