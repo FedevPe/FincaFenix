4fce63349ec709877139125e4da45607f29c1feb
Federico <127424229+FedevPe@users.noreply.github.com>
2025-06-19 16:07:47 -0300

    Creacion de interactor

---
+++ FincaFenix.UsesCases/Interactors/FarmInteractor.cs
@@ -0,0 +1,20 @@
+﻿using FincaFenix.Entities.DTOs.WorkOrderDTOs;
+using FincaFenix.Entities.POCOEntities;
+using FincaFenix.UsesCases.Interfaces.Farms;
+using FincaFenix.UsesCases.Repository;
+
+namespace FincaFenix.UsesCases.Interactors
+{
+    public class FarmInteractor(IFarmOutputPort presenter,
+        IFarmRepository repository) : IFarmInputPort
+    {
+        public async Task GetFarmById(int id)
+        {
+            await presenter.Handle(await repository.GetFarmById(id));
+        }
+        public async Task GetListFarm()
+        {
+            await presenter.HandleList(await repository.GetListFarm());
+        }
+    }
+}
