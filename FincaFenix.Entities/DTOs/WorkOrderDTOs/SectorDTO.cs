3c0ff37045e5523aed28785e492ca20901c04e52
Federico <afedeperez2021@gmail.com>
2025-06-20 21:17:44 -0300

    Modificación de DTOs

--- FincaFenix.Entities/DTOs/WorkOrderDTOs/SectorDTO.cs
+++ FincaFenix.Entities/DTOs/WorkOrderDTOs/SectorDTO.cs
@@ -3,7 +3,7 @@
     public class SectorDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public bool Selected { get; set; }
+        public string Description { get; set; }
+        public bool Selected { get; set; } = false;
     }
 }
