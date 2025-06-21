93c733301b6fda0e401e663441e20a40086a6653
Federico <afedeperez2021@gmail.com>
2025-06-20 21:17:08 -0300

    Controlador de Materiales

---
+++ FincaFenix.UsesCases/Controllers/IMaterialController.cs
@@ -0,0 +1,10 @@
+﻿using FincaFenix.Entities.DTOs.WorkOrderDTOs;
+
+namespace FincaFenix.UsesCases.Controllers
+{
+    public interface IMaterialController
+    {
+        Task<IEnumerable<MaterialDTO>> DisplayListMaterial();
+        Task<MaterialDTO> GetMaterialById(int id);
+    }
+}
