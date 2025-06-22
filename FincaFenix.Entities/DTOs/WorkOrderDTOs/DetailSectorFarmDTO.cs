15abee19f88eeac4601b33ddf7ad621c8acb7859
Federico <afedeperez2021@gmail.com>
2025-06-22 18:50:59 -0300

    DTO detalles del sector de trabajo

---
+++ FincaFenix.Entities/DTOs/WorkOrderDTOs/DetailSectorFarmDTO.cs
@@ -0,0 +1,14 @@
+﻿using FincaFenix.Entities.POCOEntities;
+
+namespace FincaFenix.Entities.DTOs.WorkOrderDTOs
+{
+    public class DetailSectorFarmDTO
+    {
+        public int Id { get; set; }
+        public int FarmId { get; set; }
+        public string SectorName { get; set; }
+        public int VarietyId { get; set; }
+        public int NumberPlants { get; set; }
+        public bool Selected { get; set; } = false;
+    }
+}
