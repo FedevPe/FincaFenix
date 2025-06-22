5508631a7a30f9ecedd85468cd28142fd35aaffe
Federico <afedeperez2021@gmail.com>
2025-06-22 18:55:29 -0300

    Modificaciones

--- FincaFenix.EFCore/DependencyContainer.cs
+++ FincaFenix.EFCore/DependencyContainer.cs
@@ -1,10 +1,4 @@
-﻿using UniversitarySystem.EFCore.Options;
-using UniversitarySystem.EFCore.Services.AddressInformation;
-using UniversitarySystem.EFCore.Services.BloodType;
-using UniversitarySystem.EFCore.Services.CollegeCareers;
-using UniversitarySystem.EFCore.Services.ProvincesAndCities;
-using UniversitarySystem.EFCore.Services.Students;
-namespace Microsoft.Extensions.DependencyInjection
+﻿namespace Microsoft.Extensions.DependencyInjection
 {
     public static class DependencyContainer
     {
