150c28b48ba8eb6e8aa876a875c6949aae8f8f58
Federico <afedeperez2021@gmail.com>
2025-07-03 10:35:03 -0300

    Update DetailWorkOrderEntity sector and add ActivityDate
    Changed the type of SectorWorked from WorkOrderWorkedSectorEntity to DetailSectorFarmEntity and added a new ActivityDate property to DetailWorkOrderEntity.

--- FincaFenix.Entities/POCOEntities/DetailWorkOrderEntity.cs
+++ FincaFenix.Entities/POCOEntities/DetailWorkOrderEntity.cs
@@ -8,10 +8,11 @@
         public int EmployeeId { get; set; }
         public EmployeeEntity Employee { get; set; }
         public int SectorWorkedId { get; set; }
-        public WorkOrderWorkedSectorEntity SectorWorked { get; set; }
+        public DetailSectorFarmEntity SectorWorked { get; set; }
         public decimal Performance { get; set; }
         public decimal WorkedHours { get; set; }
         public string Description { get; set; }
+        public DateTime ActivityDate { get; set; }
         public byte[] RowVersion { get; set; }
     }
 }
