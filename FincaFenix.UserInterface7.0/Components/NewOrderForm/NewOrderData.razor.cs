0b7fdc1937246f9a0f2772afb52374b5802c8028
Federico <afedeperez2021@gmail.com>
2025-07-10 08:28:38 -0300

    Add detailed work order components and refactor UI
    Introduced new components for work order details, including ActivityLog, DetailWorkOrderActions, DialogWorkOrderState, and GeneralInformationWorkOrder. Refactored AddDetailWorkOrderForm and DetailWorkOrder pages to use these components, improving modularity and UI consistency. Updated NewOrderData and related files to enhance date handling and sector selection. Adjusted navigation and parameter passing for work order details and actions.

--- FincaFenix.UserInterface7.0/Components/NewOrderForm/NewOrderData.razor.cs
+++ FincaFenix.UserInterface7.0/Components/NewOrderForm/NewOrderData.razor.cs
@@ -12,19 +12,10 @@ namespace FincaFenix.UserInterface7._0.Components.NewOrderForm
         public InfoNewWorkOrderViewModel ViewModel { get; set; }
 
         private List<DetailSectorFarmDTO> Sectors { get; set; } = new();
-
-        private List<DetailSectorFarmDTO> ColumnSector1 = new();
-        private List<DetailSectorFarmDTO> ColumnSector2 = new();
-        private List<DetailSectorFarmDTO> ColumnSector3 = new();
-        private List<DetailSectorFarmDTO> ColumnSector4 = new();
         
         private async Task OnFarmChanged(int farmId)
         {
             ViewModel.SelectedFarmId = farmId;
-            ColumnSector1.Clear();
-            ColumnSector2.Clear();
-            ColumnSector3.Clear();
-            ColumnSector4.Clear();
             ViewModel.SelectedSectors.Clear();
 
             if (ViewModel.SelectedFarmId != 0)
@@ -32,25 +23,6 @@ namespace FincaFenix.UserInterface7._0.Components.NewOrderForm
                 await ViewModel.LoadSectorsForFarmIdAsync(ViewModel.SelectedFarmId);
                 Sectors = ViewModel.Sectors.ToList();
                 ViewModel.SelectedFarmId = farmId;
-                for (int i = 0; i < Sectors.Count; i++)
-                {
-                    int columnIndex = i % 4;
-                    switch (columnIndex)
-                    {
-                        case 0:
-                            ColumnSector1.Add(Sectors[i]);
-                            break;
-                        case 1:
-                            ColumnSector2.Add(Sectors[i]);
-                            break;
-                        case 2:
-                            ColumnSector3.Add(Sectors[i]);
-                            break;
-                        case 3:
-                            ColumnSector4.Add(Sectors[i]);
-                            break;
-                    }
-                }
             }
             else
             {
