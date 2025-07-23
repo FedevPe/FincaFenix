76fb4e1d184b29b7ea68a46c5b4a934a41febf09
Federico <afedeperez2021@gmail.com>
2025-07-23 10:32:31 -0300

    Add guard builders and WorkOrder validator
    Introduces GuardBuilderDateTime and GuardBuilderInt for parameter validation, and adds WorkOrderValidator to enforce business rules on WorkOrderEntity instances. These changes improve validation consistency and error handling for work order creation.

---
+++ FincaFenix.Validations/Guards/GuardBuilderDateTime.cs
@@ -0,0 +1,26 @@
+﻿namespace FincaFenix.Validations.Guards
+{
+    public class GuardBuilderDateTime
+    {
+        private readonly DateTime? _value;
+        private readonly string _paramName;
+
+        public GuardBuilderDateTime(DateTime? value, string paramName) 
+        {
+            _value = value;
+            _paramName = paramName;
+        }
+        public GuardBuilderDateTime IsNull(string? message = null)
+        {
+            if (_value == null)
+                throw new Exception(message ?? $"{_paramName}: debe seleccionar una fecha valida.");
+            return this;
+        }
+        public GuardBuilderDateTime LessThan(DateTime? comparisionDate, string? message = null)
+        {
+            if (_value < comparisionDate)
+                throw new Exception(message ?? $"{_paramName} no puede ser menor a {comparisionDate}");
+            return this;
+        }
+    }
+}
---
+++ FincaFenix.Validations/Validators/WorkOrderValidator.cs
@@ -0,0 +1,36 @@
+﻿using FincaFenix.Entities.POCOEntities;
+using FincaFenix.Validations.Guards;
+
+namespace FincaFenix.Validations.Validators
+{
+    public class WorkOrderValidator
+    {
+        public static void Validate(WorkOrderEntity workOrder, bool validateIdWorkOrder = false)
+        {
+            if (workOrder == null)
+            {
+                throw new ArgumentNullException(nameof(workOrder));
+            }
+            if (validateIdWorkOrder)
+            {
+                Guard.Against(workOrder.Id, nameof(workOrder.Id)).IsEqualTo(0);
+            }
+            //Fecha de inicio de la oren
+            Guard.Against(workOrder.CreatedDate, nameof(workOrder.CreatedDate))
+                .IsNull()
+                .LessThan(DateTime.Now);
+            
+            //Finca seleccionada
+            Guard.Against(workOrder.FarmId, nameof(workOrder.FarmId))
+                .IsEqualTo(0, "La orden de trabajo debe tener una FINCA relacionada.");
+            //Tarea seleccionada
+            Guard.Against(workOrder.TaskId, nameof(workOrder.TaskId))
+                .IsEqualTo(0, "La orden de trabajo debe tener una TAREA relacionada.");
+            //Sectores seleccionados
+            foreach(var sector in workOrder.WorkedSectors)
+            {
+                Guard.Against(sector.Id, nameof(sector.Id)).IsEqualTo(0, "El sector seleccionado no es valido.");
+            }
+        }
+    }
+}
