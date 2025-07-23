f0f50515550c464fb1e43147d9960e5f2d0b37d4
Federico <afedeperez2021@gmail.com>
2025-07-23 10:31:08 -0300

    Add TotalAreaSectorService for area management
    Introduces TotalAreaSectorService to manage and notify changes in total area of sectors. Includes methods to set, sum, and subtract area values, with event notification for state changes.

---
+++ FincaFenix.UserInterface7.0/Services/TotalAreaSectorService.cs
@@ -0,0 +1,32 @@
+﻿namespace FincaFenix.UserInterface7._0.Services
+{
+    public class TotalAreaSectorService
+    {
+        public decimal? TotalAreaSectors { get; private set; } = 0; // Private set para controlar las actualizaciones
+
+        // Evento para notificar a los suscriptores cuando el área cambia
+        public event Action OnChange;
+
+        public void SetTotalAreaSectors(decimal? newArea)
+        {
+            if (TotalAreaSectors != newArea) // Solo actualizar si el valor realmente cambió
+            {
+                TotalAreaSectors = newArea;
+                NotifyStateChanged();
+            }
+        }
+        public void SumArea(decimal? area)
+        {
+            TotalAreaSectors += area;
+            SetTotalAreaSectors(TotalAreaSectors);
+            NotifyStateChanged();
+        }
+        public void SubstractArea(decimal? area)
+        {
+            TotalAreaSectors -= area;
+            SetTotalAreaSectors(TotalAreaSectors);
+            NotifyStateChanged();
+        }
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
