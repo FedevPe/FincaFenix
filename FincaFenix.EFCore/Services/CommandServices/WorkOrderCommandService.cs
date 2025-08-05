826d516d8626647bd8a44f59eefc68a1d13f04ae
Federico <afedeperez2021@gmail.com>
2025-08-05 16:06:17 -0300

    Comment out WorkedSectors processing in WorkOrderCommandService
    The code handling the assignment and saving of WorkedSectors in WorkOrderCommandService has been commented out. This may be for debugging, refactoring, or to temporarily disable the feature while investigating related issues.

--- FincaFenix.EFCore/Services/CommandServices/WorkOrderCommandService.cs
+++ FincaFenix.EFCore/Services/CommandServices/WorkOrderCommandService.cs
@@ -13,8 +13,7 @@ namespace FincaFenix.EFCore.Services.CommandServices
             var reNumber = CorrelativeNumber.FirstOrDefault(c => c.TypeDoc == "Receta");
 
             try
-            {
-                
+            {                
                 if (workOrder.Recipe != null)
                 {
                     workOrder.Recipe.NumRecipe = reNumber.LastNumber.ToString();
@@ -34,16 +33,17 @@ namespace FincaFenix.EFCore.Services.CommandServices
                 woNumber.LastNumber++;
                 await SaveChangesAsync();
 
-                if (workOrder.WorkedSectors != null && workOrder.WorkedSectors.Any())
-                {
-                    foreach (var sector in workOrder.WorkedSectors)
-                    {
-                        sector.WorkOrderId = workOrder.Id;
-                    }
+                //if (workOrder.WorkedSectors != null && workOrder.WorkedSectors.Any())
+                //{
+                //    foreach (var sector in workOrder.WorkedSectors)
+                //    {
+                //        sector.Id = 0;
+                //        sector.WorkOrderId = workOrder.Id;
+                //    }
 
-                    WorkOrderWorkedSectors.AddRange(workOrder.WorkedSectors);
-                    await SaveChangesAsync();
-                }
+                //    WorkOrderWorkedSectors.AddRange(workOrder.WorkedSectors);
+                //    await SaveChangesAsync();
+                //}
 
                 await transaction.CommitAsync();
                 return workOrder.Id;
