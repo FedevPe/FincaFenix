0e1e288e5a568772667b8b1895a1f5efa5c51f3c
Federico <afedeperez2021@gmail.com>
2025-08-13 08:24:48 -0300

    Create WorkOrderTable.razor.cs

---
+++ FincaFenix.UserInterface7.0/Components/WorkOrderTable.razor.cs
@@ -0,0 +1,39 @@
+using FincaFenix.Entities.DTOs.ShowWorkOrder;
+using FincaFenix.PDF;
+using FincaFenix.UserInterface7._0.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace FincaFenix.UserInterface7._0.Components
+{
+    public partial class WorkOrderTable
+    {
+        [Parameter] public IEnumerable<ShowWorkOrderDTO> WorkOrderList { get; set; }
+
+        [Inject] private PDFService PDFService { get; set; }
+        [Inject] private NavigationManager NavigationManager { get; set; }
+        [Inject] private ISnackbar Snackbar { get; set; }
+
+        public void OpenActivityForm(string woSelectedStatus, int idWorkOrder)
+        {
+            if (woSelectedStatus != "Cerrado")
+            {
+                NavigationManager.NavigateTo($"/actividad/registrar/{idWorkOrder}");
+            }
+            else
+            {
+                Snackbar.Add($"La orden de trabajo N° {idWorkOrder} no se encuentra activa.", MudBlazor.Severity.Warning);
+            }
+        }
+        public void OpenWorkOrderDetails(int idWorkOrder)
+        {
+            NavigationManager.NavigateTo($"/ordentrabajo/{idWorkOrder}/detalle/");
+        }
+        public async Task GeneratePDF(ShowWorkOrderDTO infoWO)
+        {
+            var document = new WorkOrderPDF(infoWO);
+            await PDFService.DownloadPdfAsync(document, $"Orden_de_trabajo_N°_{infoWO.OrderNum}");
+        }
+    }
+}
