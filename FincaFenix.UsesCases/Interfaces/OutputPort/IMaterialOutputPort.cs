0a56176e2fd7f84eeb86833ad721b9214b446282
Federico <afedeperez2021@gmail.com>
2025-08-13 08:19:48 -0300

    Update OutputPort interfaces with new DTOs and methods
    Replaced DTO types in IDetailWorkOrderOutputPort and IMaterialOutputPort to use DetailWorkOrderDTO and MaterialRecipeDTO, respectively. Refactored IWorkOrderOutputPort to update property types, add new properties, and revise method signatures to support new data structures and paginated lists.

--- FincaFenix.UsesCases/Interfaces/OutputPort/IMaterialOutputPort.cs
+++ FincaFenix.UsesCases/Interfaces/OutputPort/IMaterialOutputPort.cs
@@ -5,7 +5,7 @@ namespace FincaFenix.UsesCases.Interfaces.OutputPort
 {
     public interface IMaterialOutputPort
     {
-        List<MaterialOrderDTO> MaterialList { get; }
+        List<MaterialRecipeDTO> MaterialList { get; }
         Task HandleList(IEnumerable<MaterialEntity> materialList);
         Task Handle(MaterialEntity material);
     }
--- FincaFenix.UsesCases/Interfaces/OutputPort/IWorkOrderOutputPort.cs
+++ FincaFenix.UsesCases/Interfaces/OutputPort/IWorkOrderOutputPort.cs
@@ -1,17 +1,21 @@
-﻿using FincaFenix.Entities.DTOs.DetailWorkOrderDTO;
+﻿using FincaFenix.Entities.DTOs.ShowWorkOrder;
 using FincaFenix.Entities.DTOs.WorkOrderDTOs;
+using FincaFenix.Entities.POCOEntities;
 
 namespace FincaFenix.UsesCases.Interfaces.WorkOrder
 {
     public interface IWorkOrderOutputPort
     {
-        public ShowInfoAddActivityFormDTO WorkOrder { get; }
+        public InfoWorkOrderDTO WorkOrder { get; }
+        public ShowWorkOrderDTO WorkOrderInfo { get; }
         public IEnumerable<WorkOrderDTO> WorkOrderList { get; }
-        public IEnumerable<InfoWorkOrderDTO> InfoWorkOrderCard { get; }
+        public List<ShowWorkOrderDTO> InfoWorkOrderList { get; }
         public int TotalCount { get; }
         public bool IsSaved { get; }
-        Task Handle(int workOrderId);
-        Task Handle(ShowInfoAddActivityFormDTO workOrder);
-        Task HandleList(IEnumerable<InfoWorkOrderDTO> listDTO, int totalAcount);
+        Task Handle(int id);
+        Task Handle(InfoWorkOrderDTO workOrderId);
+        Task Handle(WorkOrderEntity workOrder);
+        Task HandleList(IEnumerable<WorkOrderEntity> listDTO);
+        Task HandleListPaginated(IEnumerable<ShowWorkOrderDTO> listDTO, int totalAcount);
     }
 }
