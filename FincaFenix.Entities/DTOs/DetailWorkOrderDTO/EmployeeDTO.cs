31f438d5243805cbca7ac7919e38f8dba478e836
Federico <afedeperez2021@gmail.com>
2025-08-27 19:32:26 -0300

    Delete EmployeeDTO.cs

--- FincaFenix.Entities/DTOs/DetailWorkOrderDTO/EmployeeDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FincaFenix.Entities.DTOs.DetailWorkOrderDTO
-{
-    public class EmployeeDTO
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? LastName { get; set; }
-    }
-}
