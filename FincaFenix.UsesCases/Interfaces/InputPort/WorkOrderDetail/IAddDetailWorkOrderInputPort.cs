1500d45e02ae904984aa96d7240fd1b5142cc640
Federico <afedeperez2021@gmail.com>
2025-08-27 19:32:53 -0300

    Create IAddDetailWorkOrderInputPort.cs

---
+++ FincaFenix.UsesCases/Interfaces/InputPort/WorkOrderDetail/IAddDetailWorkOrderInputPort.cs
@@ -0,0 +1,10 @@
+﻿using FincaFenix.Entities.DTOs.DetailWorkOrderDTO.AddDetailWorkOrder;
+
+namespace FincaFenix.UsesCases.Interfaces.InputPort.WorkOrderDetail
+{
+    public interface IAddDetailWorkOrderInputPort
+    {
+        Task AddDetailWorkOrder(AddDetailWorkOrderDTO dto);
+        Task GetActivitiesByOrderId(int orderId);
+    }
+}
