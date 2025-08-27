c4ce1a1606b2d8e3b5a1539ed811816f8664b105
Federico <afedeperez2021@gmail.com>
2025-08-27 19:28:55 -0300

    Update Interface and codebehind

--- FincaFenix.UserInterface7.0/Pages/DetailWorkOrder.razor.cs
+++ FincaFenix.UserInterface7.0/Pages/DetailWorkOrder.razor.cs
@@ -1,12 +1,14 @@
 using FincaFenix.UserInterface7._0.Services;
 using FincaFenix.ViewModels.ViewModels.WorkOrder.GetInformationWorkOrder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace FincaFenix.UserInterface7._0.Pages
 {
     public partial class DetailWorkOrder
     {
         [Inject] private GeneralInfoWorkOrderViewModel ViewModel { get; set; }
+        [Inject] private IJSRuntime JSRuntime { get; set; }
         [Inject] private LoadDetailsWorkOrderViewModel DetailsVM { get; set; }
         [Inject] public TextAppBarStateService TextApp { get; set; }
         [Inject] public NavigationManager NavigationManager { get; set; }
@@ -32,14 +34,22 @@ namespace FincaFenix.UserInterface7._0.Pages
         public enum WorkOrderTab
         {
             Activities,
-            Recipe
+            Recipe,
+            Consumption
         }
 
         private WorkOrderTab activeTab = WorkOrderTab.Activities;
 
-        private void SetActiveTab(WorkOrderTab tab)
+        private async Task SetActiveTab(WorkOrderTab tab)
         {
             activeTab = tab;
+            await Task.Delay(50);
+            await ScrollToElement("data");            
+        }
+
+        private async Task ScrollToElement(string elementId)
+        {
+            await JSRuntime.InvokeVoidAsync("scrollToElement", elementId);
         }
 
         private string GetTabActiveClass(WorkOrderTab tab)
