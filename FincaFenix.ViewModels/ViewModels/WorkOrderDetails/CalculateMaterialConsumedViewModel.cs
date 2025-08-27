4d54391726d9277eca5b01af4855dc3de077ce1a
Federico <afedeperez2021@gmail.com>
2025-08-27 19:25:55 -0300

    Create CalculateMaterialConsumeViewModel

---
+++ FincaFenix.ViewModels/ViewModels/WorkOrderDetails/CalculateMaterialConsumedViewModel.cs
@@ -0,0 +1,29 @@
+﻿using FincaFenix.Entities.DTOs.DetailWorkOrderDTO.GetDetailWorkOrder;
+using FincaFenix.Entities.DTOs.RecipeDTO;
+
+namespace FincaFenix.ViewModels.ViewModels.WorkOrderDetails
+{
+    public class CalculateMaterialConsumedViewModel
+    {
+        public decimal TotalPerfomance { get; set; }
+        public async Task CalculateTotalAmountConsume(RecipeWorkOrderDTO recipeWorkOrder, IEnumerable<DetailWorkOrderDTO> detailWorkOrderList)
+        {
+            TotalPerfomance = detailWorkOrderList.Sum(d => d.Performance);
+
+            foreach (var detail in recipeWorkOrder.Details)
+            {
+                detail.TotalAmountConsumed = CalculateTotalAmount(detail, TotalPerfomance, recipeWorkOrder.TRV);
+            }
+            await Task.CompletedTask;
+        }
+
+        private decimal CalculateTotalAmount(DetailRecipeDTO detail, decimal totalPerformance, decimal trv)
+        {
+            var amountRequired = (detail.AmountRequiredUnit == "cc" || detail.AmountRequiredUnit == "gr")
+                ? detail.AmountRequired / 1000m
+                : detail.AmountRequired;
+
+            return (totalPerformance * amountRequired) / trv;
+        }
+    }
+}
