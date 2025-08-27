015078ecb35983fcaedc6f5cbc35553f4ddb8033
Federico <afedeperez2021@gmail.com>
2025-08-27 19:32:33 -0300

    Create AddDetailWorkOrderDTO.cs

---
+++ FincaFenix.Entities/DTOs/DetailWorkOrderDTO/AddDetailWorkOrder/AddDetailWorkOrderDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FincaFenix.Entities.DTOs.DetailWorkOrderDTO.AddDetailWorkOrder
+{
+    public class AddDetailWorkOrderDTO
+    {
+        public int OrderId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime? ActivityDate { get; set; }
+        public InfoDetailWorkOrderDTO Info { get; set; } = new InfoDetailWorkOrderDTO();
+
+    }
+}
