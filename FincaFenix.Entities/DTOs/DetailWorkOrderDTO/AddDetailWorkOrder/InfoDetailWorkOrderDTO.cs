2b13cab30169de5bf2444fa76a4eb2aea98da13a
Federico <afedeperez2021@gmail.com>
2025-08-27 19:32:31 -0300

    Create InfoDetailWorkOrderDTO.cs

---
+++ FincaFenix.Entities/DTOs/DetailWorkOrderDTO/AddDetailWorkOrder/InfoDetailWorkOrderDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FincaFenix.Entities.DTOs.DetailWorkOrderDTO.AddDetailWorkOrder
+{
+    public class InfoDetailWorkOrderDTO
+    {
+        public int SectorWorkedId { get; set; }
+        public decimal Performance { get; set; }
+        public decimal WorkedHours { get; set; }
+        public string Description { get; set; }
+    }
+}
