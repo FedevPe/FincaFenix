894a9781a9ae00fc619b0441b1ab42f19fd3044c
Federico <afedeperez2021@gmail.com>
2025-11-21 16:20:34 -0300

    Add interfaces for work order detail operations
    Introduces IDeleteDetailWorkOrder and IUpdateDetailWorkOrder interfaces for handling deletion and update operations on work order details. Also updates .gitignore to exclude appsettings.json from FincaFenix.UserInterface7.0.

---
+++ FincaFenix.UsesCases/Interfaces/InputPort/WorkOrderDetail/IUpdateDetailWorkOrder.cs
@@ -0,0 +1,7 @@
+﻿namespace FincaFenix.UsesCases.Interfaces.InputPort.WorkOrderDetail
+{
+    public interface IUpdateDetailWorkOrder
+    {
+        Task UpdateDetailWorkOrder(int orderId, int employeeId, DateTime activityDate);
+    }
+}
